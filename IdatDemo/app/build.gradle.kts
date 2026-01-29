72c27753b5e941931ca65bf135d4b0f8430e3e5c
Steven-Abanto <steven.abanto.xd@gmail.com>
2026-01-28 22:34:49 -0500

    Se añaden enlaces (intents)

--- IdatDemo/app/build.gradle.kts
+++ IdatDemo/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.idatdemo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.idatdemo"
--- IdatDemo/app/src/main/java/com/example/idatdemo/MainActivity.kt
+++ IdatDemo/app/src/main/java/com/example/idatdemo/MainActivity.kt
@@ -1,22 +1,53 @@
 package com.example.idatdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvRegistro : TextView
+    private lateinit var ivWeb : ImageView
+    private lateinit var ivTelefono : ImageView
+    private lateinit var ivMensaje : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+//      val tvRegistro : TextView = findViewById(R.id.tvRegistro)
+        tvRegistro = findViewById(R.id.tvRegistro)
 
-        val tvRegistro : TextView = findViewById(R.id.tvRegistro)
+        ivWeb = findViewById(R.id.ivWeb)
+        ivTelefono = findViewById(R.id.ivTelefono)
+        ivMensaje = findViewById(R.id.ivMensaje)
 
-        tvRegistro.setOnClickListener(){
-            
+        ivWeb.setOnClickListener(){
+            val intent : Intent = Intent(Intent.ACTION_VIEW)
+            intent.setData("https://www.starwars.com/".toUri())
+            startActivity(intent)
+        }
+
+        ivTelefono.setOnClickListener(){
+            val intent : Intent = Intent(Intent.ACTION_CALL)
+            intent.setData("tel:+51999999999".toUri())
+            startActivity(intent)
+        }
+
+        ivMensaje.setOnClickListener(){
+            val intent : Intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
+
+        tvRegistro.setOnClickListener{
+            val intent : Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
