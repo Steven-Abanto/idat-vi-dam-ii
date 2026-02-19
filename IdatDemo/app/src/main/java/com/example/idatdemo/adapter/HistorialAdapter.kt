f5c5d0657f2e017e4cd3f05cd6a3916fbcc0c710
Steven-Abanto <steven.abanto.xd@gmail.com>
2026-02-18 22:34:46 -0500

    Historial Adapter avanzado

--- IdatDemo/app/src/main/java/com/example/idatdemo/adapter/HistorialAdapter.kt
+++ IdatDemo/app/src/main/java/com/example/idatdemo/adapter/HistorialAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.idatdemo.adapter
 
-class HistorialAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.idatdemo.R
+import com.example.idatdemo.entity.Compra
+
+class HistorialAdapter(private val listaHistorial : List<Compra>)
+    : RecyclerView.Adapter<HistorialAdapter.HistorialViewHolder>() {
+    inner class HistorialViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val ivFoto : ImageView = itemView.findViewById(R.id.ivFoto)
+        val tvId : TextView = itemView.findViewById(R.id.tvId)
+        val tvProducto : TextView = itemView.findViewById(R.id.tvProducto)
+        val tvFecha : TextView = itemView.findViewById(R.id.tvFecha)
+    }
+
+    //Creación de plantilla - rellena valores
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistorialViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_historial,parent,false)
+        return HistorialViewHolder(view)
+    }
+
+    //Cuántos elementos van a visualizarse
+    override fun getItemCount(): Int {
+        return listaHistorial.size
+    }
+
+    //Renderiza todos los elementos
+    override fun onBindViewHolder(holder: HistorialViewHolder, position: Int) {
+        val compra = listaHistorial[position]
+        holder.tvId.text = "ID: #${compra.id}"
+        holder.tvProducto.text = "Producto: #${compra.producto}"
+        holder.tvFecha.text = "Fecha: #${compra.fecha}"
+    }
+
 }
---
+++ IdatDemo/app/src/main/java/com/example/idatdemo/entity/Compra.kt
@@ -0,0 +1,8 @@
+package com.example.idatdemo.entity
+
+data class Compra (
+    var id : String = "",
+    var imagen : String = "",
+    var producto : String = "",
+    var fecha : String = ""
+)
